c2f07dba8538e42ff3009216c78fa29d1e25efdc
Niangniang <mountagha.tmn@gmail.com>
2021-11-17 16:16:58 +0100

    base

---
+++ src/app/services/event_driver.service.ts
@@ -0,0 +1,15 @@
+import {Inject, Injectable} from "@angular/core";
+import {ActionEvent} from "../state/product.state";
+import {Subject} from "rxjs";
+
+@Injectable({providedIn: "root"})
+export class EventDriverService{
+  sourceEventSubject: Subject<ActionEvent> = new Subject<ActionEvent>();
+  sourceEventSubjectObservable = this.sourceEventSubject.asObservable();
+
+  publishEvent(event : ActionEvent){
+    this.sourceEventSubject.next(event);
+
+  }
+
+}
